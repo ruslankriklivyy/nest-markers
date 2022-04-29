b780487bc72a8be92f97a07ed7f816d07b72e645
ruslan-quartsoft <ruslan@quartsoft.com>
2022-04-29 17:41:05 +0300

    feat: vercel.json

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,7 @@
 import * as cookieParser from 'cookie-parser';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import express from 'express';
 import { ValidationPipe } from '@nestjs/common';
-
-const app = express();
-app.set('trust proxy', 1);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
