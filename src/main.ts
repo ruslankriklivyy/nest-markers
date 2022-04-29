1cca0be5c120344cbe3b97ac1872a71f4e8dbdc9
ruslan-quartsoft <ruslan@quartsoft.com>
2022-04-29 15:34:24 +0300

    feat: procfile

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,17 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
-  // {
+  // app.enableCors({
   //   credentials: true,
-  //     origin: true,
+  //   origin: true,
   //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  // }
+  // });
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
   await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
