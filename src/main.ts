e46851bb6d3c3cabf67059c8d59691ebec9652e4
ruslan-quartsoft <ruslan@quartsoft.com>
2022-04-29 17:47:09 +0300

    feat: vercel.json

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import * as cookieParser from 'cookie-parser';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,17 @@ async function bootstrap() {
     origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET || 'Super Secret (change it)',
+      resave: true,
+      saveUninitialized: false,
+      cookie: {
+        sameSite: 'none', // must be 'none' to enable cross-site delivery
+        secure: true, // must be true if sameSite='none'
+      },
+    }),
+  );
   await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
