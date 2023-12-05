f05f4aa9f30c405f3e60a480caa6ad9553a88c3d
ruslan-quartsoft <ruslan@quartsoft.com>
2023-12-05 15:22:53 +0200

    fix: added providers

--- src/modules/custom-field/custom-field.module.ts
+++ src/modules/custom-field/custom-field.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { CustomFieldService } from './custom-field.service';
 import { CustomFieldController } from './custom-field.controller';
 import { DatabaseModule } from '@/modules/database/database.module';
+import { customFieldProviders } from '@/modules/custom-field/custom-field.providers';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [CustomFieldController],
-  providers: [CustomFieldService],
+  providers: [CustomFieldService, ...customFieldProviders],
 })
 export class CustomFieldModule {}
